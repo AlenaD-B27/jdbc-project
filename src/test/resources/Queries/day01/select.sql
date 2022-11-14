77f74dda1d3ef46a9dafe33e306609d16242cae0
AlenaD-B27 <lenador.18@gmail.com>
2022-11-14 13:39:49 -0600

    select manipulations

---
+++ src/test/resources/Queries/day01/select.sql
@@ -0,0 +1,27 @@
+select * from employees;
+/*
+ multiple lines comment
+ */
+select * from employees;
+-- single line comment;
+-- SQL language is NOT case sensitive;
+select * FROM eMployEEs;
+
+-- display all info from departments table;
+
+select * from DEPARTMENTS;
+
+-- display firstname from employees tbl;
+
+select FIRST_NAME from EMPLOYEES;
+
+-- display firstname, lastname and salary from employees tbl;
+
+select FIRST_NAME , LAST_NAME , SALARY from EMPLOYEES;
+
+select COUNTRY_NAME, REGION_ID from COUNTRIES;
+
+select JOB_ID, JOB_TITLE from JOBS;
+
+
+
