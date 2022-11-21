556427183f23403f01a951762b5922393cd92855
AlenaD-B27 <lenador.18@gmail.com>
2022-11-21 11:51:20 -0600

    get table data navigation

--- src/test/java/com/cydeo/jdbctests/TestOracleConnection.java
+++ src/test/java/com/cydeo/jdbctests/TestOracleConnection.java
@@ -19,9 +19,51 @@ public class TestOracleConnection {
         // stores data after execution. ONLY execution data, no table info
         ResultSet rs = statement.executeQuery("select * from regions");
 
+        // GETTING DATA
+
+        // next() ---> Moves the cursor forward one row from its current position
+        rs.next();
+        // pointer shows the first row
+
+        // how to get data
+
+        // rs.getInt(index) --> returns integer. Index start from 1 in SQL, it refers column position
+
+        System.out.println(rs.getInt(1));
+
+        // getString() --> same, just returns String
+
+        System.out.println(rs.getString(1));
+
+        //-------------------------------------------------
+
+        System.out.println(rs.getInt("REGION_ID"));
+        System.out.println(rs.getString("REGION_ID"));
+        System.out.println(rs.getString(2));
+
+        /*
+        System.out.println("---------- 2nd row: -------------");
+
+        rs.next();
+        System.out.println(rs.getString(1) + " - " + rs.getString(2));
 
 
+        System.out.println("---------- 3rd row: -------------");
 
+        rs.next();
+        System.out.println(rs.getString(1) + " - " + rs.getString(2));
+
+        System.out.println("---------- 4th row: -------------");
+
+        rs.next();
+        System.out.println(rs.getString(1) + " - " + rs.getString(2));
+
+         */
+
+        System.out.println("---------- Table rows 2-to-the-end -------------");
+        while(rs.next()){
+            System.out.println(rs.getString(1) + " - " + rs.getString(2));
+        }
 
 
         // close connections
